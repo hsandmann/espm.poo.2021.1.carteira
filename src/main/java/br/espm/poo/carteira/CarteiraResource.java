e706a23ba6950134b9377d088ae89b60b6dffd51
Humberto Sandmann <hsandmann@ieee.org>
2021-06-10 12:35:58 -0300

    Transaciona cambio: venda e compra

--- src/main/java/br/espm/poo/carteira/CarteiraResource.java
+++ src/main/java/br/espm/poo/carteira/CarteiraResource.java
@@ -2,13 +2,18 @@ package br.espm.poo.carteira;
 
 import br.espm.poo.cambio.common.controller.CambioController;
 import br.espm.poo.cambio.common.datatype.Cotacao;
+import br.espm.poo.cambio.common.datatype.Moeda;
 import br.espm.poo.carteira.common.controller.CarteiraController;
 import br.espm.poo.carteira.common.datatype.Carteira;
+import br.espm.poo.carteira.common.datatype.TransacaoBean;
 import br.espm.poo.carteira.common.datatype.TransacaoCambio;
+import br.espm.poo.carteira.common.datatype.TransacaoTipo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -19,33 +24,36 @@ import java.util.UUID;
 public class CarteiraResource implements CarteiraController {
 
     @Autowired
-    private CambioController cambioController;
+    private CarteiraService carteiraService;
+
+    @Autowired
+    private TransacaoCambioService transacaoCambioService;
+
+    @Override
+    public List<Carteira> carteiras() {
+        return null;
+    }
 
     @Override
     public Carteira carteira(String id) {
-        Carteira carteira = new Carteira();
+        return carteiraService.findBy(id);
+    }
 
-        Cotacao cotacao = new Cotacao();
-        cotacao.setId(UUID.randomUUID().toString());
+    @Override
+    public Carteira create(Carteira carteira) {
+        Carteira c = new Carteira();
+        c.setSaldo(BigDecimal.ZERO);
+        return carteiraService.create(c);
+    }
 
-        // Magica tem que acontecer
-        cotacao.setMoeda(cambioController.moedas().get(0));
+    @Override
+    public TransacaoCambio cambioComprar(String idCarteira, TransacaoBean bean) {
+        return transacaoCambioService.comprar(idCarteira, bean);
+    }
 
-
-        cotacao.setData(new Date());
-        cotacao.setValor(6.7);
-
-        TransacaoCambio tCambio = new TransacaoCambio();
-        tCambio.setCarteira(null); // HATEOS
-        tCambio.setData(new Date());
-        tCambio.setCotacao(cotacao);
-        tCambio.setQuantidade(100.0);
-
-        List<TransacaoCambio> listTransacoesCambio = new ArrayList<>();
-        listTransacoesCambio.add(tCambio);
-
-        carteira.setTransacoesCambio(listTransacoesCambio);
-        return carteira;
+    @Override
+    public TransacaoCambio cambioVender(String idCarteira, TransacaoBean bean) {
+        return transacaoCambioService.vender(idCarteira, bean);
     }
 
 }
